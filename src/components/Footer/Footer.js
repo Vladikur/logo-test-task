903d86a84ebcb490789b49ae5734d077c64fb6f6
Vladislav Kurmaev <Vladikurmav@yandex.ru>
2022-04-27 00:31:26 +0300

    end footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,4 +1,8 @@
 import logo from '../../images/footer-logo.svg';
+import telegram from '../../images/telegram.svg';
+import vk from '../../images/vk.svg';
+import twitter from '../../images/twitter.svg';
+import h from '../../images/h.svg';
 
 
 const Footer = () => {
@@ -7,15 +11,22 @@ const Footer = () => {
   return (
     <footer className="footer">
       <img src={logo} alt="Логотип" className="footer__logo"/>
-      <p className="footer__copyright">&copy; ООО «Лого», 2008—2022</p>
+      <div className="footer__icons">
+        <img src={telegram} alt="Иконка" className="footer__icon"/>
+        <img src={vk} alt="Иконка" className="footer__icon"/>
+        <img src={twitter} alt="Иконка" className="footer__icon"/>
+        <img src={h} alt="Иконка" className="footer__icon"/>
+      </div>
       <nav>
         <ul className="footer__links">
           <li><a href="foo" className="footer__link">Решения</a></li>
           <li><a href="foo" className="footer__link">Оборудование</a></li>
           <li><a href="foo" className="footer__link">О компании</a></li>
-          <li><a href="foo" className="footer__link">Блог</a></li>
+          <li><a href="foo" className="footer__link footer__link_child_last">Блог</a></li>
        </ul>
       </nav>
+      <p className="footer__copyright">&copy; ООО «Лого», 2008—2022</p>
+      <p className="footer__copyright">Лицензия на телематические услуги № 176267</p>
     </footer>
   );
 }
